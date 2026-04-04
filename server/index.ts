a160b80875c02175735b0ea198c41f2b31d7a738
tasneem19927-oss <tasneem1992.7@gmail.com>
2026-04-04 20:42:23 +0300

    Replace registerRoutes with createRoutes in server

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes";
+import { createRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
 
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
 });
 
 (async () => {
-  await registerRoutes(httpServer, app);
+        createRoutes(app, httpServer);
 
   app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
