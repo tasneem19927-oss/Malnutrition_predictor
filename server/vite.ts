3e8ce058302cca4e0a75ed9cb6af5011c2935d90
tasneem19927-oss <tasneem1992.7@gmail.com>
2026-04-07 21:03:46 +0300

    vite.ts

--- server/vite.ts
+++ server/vite.ts
@@ -1,6 +1,6 @@
-import { type Express } from "express";
+import type { Express } from "express";
 import { createServer as createViteServer, createLogger } from "vite";
-import { type Server } from "http";
+import type { Server } from "http";
 import viteConfig from "../vite.config";
 import fs from "fs";
 import path from "path";
@@ -42,13 +42,15 @@ export async function setupVite(server: Server, app: Express) {
         "index.html",
       );
 
-      // always reload the index.html file from disk incase it changes
       let template = await fs.promises.readFile(clientTemplate, "utf-8");
+
       template = template.replace(
         `src="/src/main.tsx"`,
         `src="/src/main.tsx?v=${nanoid()}"`,
       );
+
       const page = await vite.transformIndexHtml(url, template);
+
       res.status(200).set({ "Content-Type": "text/html" }).end(page);
     } catch (e) {
       vite.ssrFixStacktrace(e as Error);
