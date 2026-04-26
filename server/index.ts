66337a7a7114e3ab8218836f137fe5d3aa3f70c2
tasneem19927-oss <tasneem1992.7@gmail.com>
2026-04-26 23:27:06 +0300

    fix: rename createRoutes to registerRoutes in server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
-import { createRoutes } from "./routes";
+import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
 
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
 });
 
 (async () => {
-        createRoutes(app, httpServer);
+        await registerRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
